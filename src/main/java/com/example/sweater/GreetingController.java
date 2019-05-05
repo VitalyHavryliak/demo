31b9ec9f51a46a810c05abfb0f8e25572b2f4ba7
vitalii.havryliak <vitalii.havryliak@wds.co>
2019-05-06 00:23:18 +0300

    AdKi: little update:)

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -1,7 +1,6 @@
 package com.example.sweater;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,6 +8,7 @@ import java.util.Map;
 
 @Controller
 public class GreetingController {
+
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name="name", required=false, defaultValue="World") String name,
